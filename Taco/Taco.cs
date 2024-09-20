e8d7f32169088cf9bef24983cc38f3062024685d
pizzafaceopossum <pizzafaceopossum@gmail.com>
2024-09-20 15:35:06 -0400

    Changed the name of the mod and prepared voided copy of the item

--- Taco/Taco.cs
+++ Taco/Taco.cs
@@ -4,7 +4,7 @@ using RoR2;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 
-namespace ExamplePlugin
+namespace Taco
 {
     // This is an example plugin that can be put in
     // BepInEx/plugins/ExamplePlugin/ExamplePlugin.dll to test out.
@@ -29,7 +29,7 @@ namespace ExamplePlugin
     // BaseUnityPlugin itself inherits from MonoBehaviour,
     // so you can use this as a reference for what you can declare and use in your plugin class
     // More information in the Unity Docs: https://docs.unity3d.com/ScriptReference/MonoBehaviour.html
-    public class ExamplePlugin : BaseUnityPlugin
+    public class Taco : BaseUnityPlugin
     {
         // The Plugin GUID should be a unique ID for this plugin,
         // which is human readable (as it is used in places like the config).
@@ -37,12 +37,14 @@ namespace ExamplePlugin
         // we will deprecate this mod.
         // Change the PluginAuthor and the PluginName !
         public const string PluginGUID = PluginAuthor + "." + PluginName;
-        public const string PluginAuthor = "AuthorName";
-        public const string PluginName = "ExamplePlugin";
+        public const string PluginAuthor = "pizzafaceopossum";
+        public const string PluginName = "Taco";
         public const string PluginVersion = "1.0.0";
 
         // We need our item definition to persist through our functions, and therefore make it a class field.
-        private static ItemDef myItemDef;
+        private static ItemDef tacoItemDef;
+        private static ItemDef voidTacoItemDef;
+        
 
         // The Awake() method is run at the very start when the game is initialized.
         public void Awake()
@@ -51,54 +53,71 @@ namespace ExamplePlugin
             Log.Init(Logger);
 
             // First let's define our item
-            myItemDef = ScriptableObject.CreateInstance<ItemDef>();
+            tacoItemDef = ScriptableObject.CreateInstance<ItemDef>();
+            voidTacoItemDef = ScriptableObject.CreateInstance<ItemDef>();
 
             // Language Tokens, explained there https://risk-of-thunder.github.io/R2Wiki/Mod-Creation/Assets/Localization/
-            myItemDef.name = "EXAMPLE_CLOAKONKILL_NAME";
-            myItemDef.nameToken = "EXAMPLE_CLOAKONKILL_NAME";
-            myItemDef.pickupToken = "EXAMPLE_CLOAKONKILL_PICKUP";
-            myItemDef.descriptionToken = "EXAMPLE_CLOAKONKILL_DESC";
-            myItemDef.loreToken = "EXAMPLE_CLOAKONKILL_LORE";
+            tacoItemDef.name = "EXAMPLE_taco_NAME";
+            tacoItemDef.nameToken = "EXAMPLE_taco_NAME_TOKEN";
+            tacoItemDef.pickupToken = "EXAMPLE_taco_PICKUP";
+            tacoItemDef.descriptionToken = "EXAMPLE_taco_DESC";
+            tacoItemDef.loreToken = "EXAMPLE_taco_LORE";
+
+            voidTacoItemDef.name = "EXAMPLE_voidtaco_NAME";
+            voidTacoItemDef.nameToken = "EXAMPLE_voidtaco_NAME_TOKEN";
+            voidTacoItemDef.pickupToken = "EXAMPLE_voidtaco_PICKUP";
+            voidTacoItemDef.descriptionToken = "EXAMPLE_voidtaco_DESC";
+            voidTacoItemDef.loreToken = "EXAMPLE_voidtaco_LORE";
 
             // The tier determines what rarity the item is:
             // Tier1=white, Tier2=green, Tier3=red, Lunar=Lunar, Boss=yellow,
             // and finally NoTier is generally used for helper items, like the tonic affliction
 #pragma warning disable Publicizer001 // Accessing a member that was not originally public. Here we ignore this warning because with how this example is setup we are forced to do this
-            myItemDef._itemTierDef = Addressables.LoadAssetAsync<ItemTierDef>("RoR2/Base/Common/Tier2Def.asset").WaitForCompletion();
+            tacoItemDef._itemTierDef = Addressables.LoadAssetAsync<ItemTierDef>("RoR2/Base/Common/Tier1Def.asset").WaitForCompletion();
+            voidTacoItemDef._itemTierDef = Addressables.LoadAssetAsync<ItemTierDef>("RoR2/DLC1/Common/VoidTier1Def.asset").WaitForCompletion();
 #pragma warning restore Publicizer001
             // Instead of loading the itemtierdef directly, you can also do this like below as a workaround
             // myItemDef.deprecatedTier = ItemTier.Tier2;
 
             // You can create your own icons and prefabs through assetbundles, but to keep this boilerplate brief, we'll be using question marks.
-            myItemDef.pickupIconSprite = Addressables.LoadAssetAsync<Sprite>("RoR2/Base/Common/MiscIcons/texMysteryIcon.png").WaitForCompletion();
-            myItemDef.pickupModelPrefab = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Mystery/PickupMystery.prefab").WaitForCompletion();
+            tacoItemDef.pickupIconSprite = Addressables.LoadAssetAsync<Sprite>("RoR2/Base/Common/MiscIcons/texMysteryIcon.png").WaitForCompletion();
+            tacoItemDef.pickupModelPrefab = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Mystery/PickupMystery.prefab").WaitForCompletion();
+            
+            voidTacoItemDef.pickupIconSprite = Addressables.LoadAssetAsync<Sprite>("RoR2/Base/Common/MiscIcons/texMysteryIcon.png").WaitForCompletion();
+            voidTacoItemDef.pickupModelPrefab = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Mystery/PickupMystery.prefab").WaitForCompletion();
 
             // Can remove determines
             // if a shrine of order,
             // or a printer can take this item,
             // generally true, except for NoTier items.
-            myItemDef.canRemove = true;
+            tacoItemDef.canRemove = true;
+            voidTacoItemDef.canRemove = false;
 
             // Hidden means that there will be no pickup notification,
             // and it won't appear in the inventory at the top of the screen.
             // This is useful for certain noTier helper items, such as the DrizzlePlayerHelper.
-            myItemDef.hidden = false;
+            tacoItemDef.hidden = false;
+            voidTacoItemDef.hidden = false;
 
             // You can add your own display rules here,
             // where the first argument passed are the default display rules:
             // the ones used when no specific display rules for a character are found.
             // For this example, we are omitting them,
             // as they are quite a pain to set up without tools like https://thunderstore.io/package/KingEnderBrine/ItemDisplayPlacementHelper/
-            var displayRules = new ItemDisplayRuleDict(null);
+            var taco_displayRules = new ItemDisplayRuleDict(null);
+            var voidTaco_displayRules = new ItemDisplayRuleDict(null);
+            
 
             // Then finally add it to R2API
-            ItemAPI.Add(new CustomItem(myItemDef, displayRules));
+            ItemAPI.Add(new CustomItem(tacoItemDef, taco_displayRules));
+            ItemAPI.Add(new CustomItem(voidTacoItemDef, voidTaco_displayRules));
 
             // But now we have defined an item, but it doesn't do anything yet. So we'll need to define that ourselves.
-            GlobalEventManager.onCharacterDeathGlobal += GlobalEventManager_onCharacterDeathGlobal;
+            GlobalEventManager.onServerDamageDealt += taco_onServerDamageDealt;
+            GlobalEventManager.onServerDamageDealt += voidTaco_onServerDamageDealt;
         }
 
-        private void GlobalEventManager_onCharacterDeathGlobal(DamageReport report)
+        private void taco_onServerDamageDealt(DamageReport report)
         {
             // If a character was killed by the world, we shouldn't do anything.
             if (!report.attacker || !report.attackerBody)
@@ -112,14 +131,40 @@ namespace ExamplePlugin
             if (attackerCharacterBody.inventory)
             {
                 // Store the amount of our item we have
-                var garbCount = attackerCharacterBody.inventory.GetItemCount(myItemDef.itemIndex);
+                var garbCount = attackerCharacterBody.inventory.GetItemCount(tacoItemDef.itemIndex);
                 if (garbCount > 0 &&
                     // Roll for our 50% chance.
                     Util.CheckRoll(50, attackerCharacterBody.master))
                 {
                     // Since we passed all checks, we now give our attacker the cloaked buff.
                     // Note how we are scaling the buff duration depending on the number of the custom item in our inventory.
-                    attackerCharacterBody.AddTimedBuff(RoR2Content.Buffs.Cloak, 3 + garbCount);
+                    attackerCharacterBody.AddTimedBuff(RoR2Content.Buffs.LifeSteal, 3 + garbCount);
+                }
+            }
+        }
+
+        private void voidTaco_onServerDamageDealt(DamageReport report)
+        {
+            // If a character was killed by the world, we shouldn't do anything.
+            if (!report.attacker || !report.attackerBody)
+            {
+                return;
+            }
+
+            var attackerCharacterBody = report.attackerBody;
+
+            // We need an inventory to do check for our item
+            if (attackerCharacterBody.inventory)
+            {
+                // Store the amount of our item we have
+                var garbCount = attackerCharacterBody.inventory.GetItemCount(voidTacoItemDef.itemIndex);
+                if (garbCount > 0 &&
+                    // Roll for our 50% chance.
+                    Util.CheckRoll(50, attackerCharacterBody.master))
+                {
+                    // Since we passed all checks, we now give our attacker the cloaked buff.
+                    // Note how we are scaling the buff duration depending on the number of the custom item in our inventory.
+                    attackerCharacterBody.AddTimedBuff(RoR2Content.Buffs.OnFire, 3 + garbCount);
                 }
             }
         }
@@ -135,8 +180,18 @@ namespace ExamplePlugin
 
                 // And then drop our defined item in front of the player.
 
-                Log.Info($"Player pressed F2. Spawning our custom item at coordinates {transform.position}");
-                PickupDropletController.CreatePickupDroplet(PickupCatalog.FindPickupIndex(myItemDef.itemIndex), transform.position, transform.forward * 20f);
+                Log.Info($"Player pressed F2. Spawning taco at coordinates {transform.position}");
+                PickupDropletController.CreatePickupDroplet(PickupCatalog.FindPickupIndex(tacoItemDef.itemIndex), transform.position, transform.forward * 20f);
+            }
+            if (Input.GetKeyDown(KeyCode.F3))
+            {
+                // Get the player body to use a position:
+                var transform = PlayerCharacterMasterController.instances[0].master.GetBodyObject().transform;
+
+                // And then drop our defined item in front of the player.
+
+                Log.Info($"Player pressed F3. Spawning void taco at coordinates {transform.position}");
+                PickupDropletController.CreatePickupDroplet(PickupCatalog.FindPickupIndex(voidTacoItemDef.itemIndex), transform.position, transform.forward * 20f);
             }
         }
     }
